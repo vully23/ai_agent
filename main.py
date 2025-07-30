824e977e4c208fe251484c5f75e7265833f6476f
vully23 <vullynerable@gmail.com>
2025-07-30 18:26:06 -0400

    message history and --verbose flag

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     if len(sys.argv) == 1:
         print('No prompt provided. Use format [main.py "Your_prompt_here"]')
         sys.exit(1)
-    user_prompt = " ".join(sys.argv[1:])
+    
+    if sys.argv[-1] == "--verbose":
+        verbose = True
+        user_prompt = " ".join(sys.argv[1:-1])
+    else:
+        verbose = False
+        user_prompt = " ".join(sys.argv[1:])
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-
-    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = user_prompt)
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Response: {response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
